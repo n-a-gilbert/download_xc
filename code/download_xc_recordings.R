bebb255b8d649d3e0141f4fb364966e6475532ae
Gilbert <neil.gilbert@okstate.edu>
2024-11-20 13:38:35 -0600

    hahaha

--- code/download_xc_recordings.R
+++ code/download_xc_recordings.R
@@ -31,7 +31,8 @@ sci_names_to_query <- d |>
   dplyr::summarise( med = median(perc)) |> 
   # I'm going to retain species that are reported on at least 1% of checklists
   dplyr::filter(med > 0.01) |> 
-  dplyr::arrange(-med)
+  dplyr::arrange(-med) |> 
+  dplyr::mutate( nslice = ceiling(med * 10))
 
 sci_names_vector <- sci_names_to_query |> 
   dplyr::pull(sci)
@@ -44,4 +45,41 @@ for(i in 1:length(sci_names_vector)){
   print( paste("Finished", i, "of", length(sci_names_vector)))
 }
 
-do.call(rbind, kk)
+kk_metadata <- dplyr::bind_rows(kk)
+
+download_these <- kk_metadata |>
+  # the call type column is a mess. Simplify to song, call, song and call, and other
+  mutate( song = ifelse(grepl("song", Vocalization_type), 1, 0),
+          call = ifelse(grepl("call", Vocalization_type), 1, 0)) |> 
+  dplyr::mutate( type = ifelse(song == 1 & call == 0, "song", 
+                               ifelse(song == 0 & call == 1, "call", 
+                                      ifelse(song == 1 & call == 1, "song & call", "other")))) |> 
+  # omit recordings without a quality score
+  dplyr::filter(!Quality == "no score") |> 
+  # convert to a number for sorting
+  dplyr::mutate(qual = as.numeric(factor(Quality))) |>
+  # get length of recording in seconds
+  tidyr::separate( Length, into = c("minutes", "seconds"), sep = ":") |> 
+  dplyr::mutate(across(minutes:seconds, function(x) as.numeric(x))) |> 
+  dplyr::mutate(minutes = minutes * 60) |> 
+  dplyr::mutate(len_sec = minutes + seconds) |> 
+  # filter out recordings longer than 1.5 minutes
+  dplyr::filter(len_sec < 90) |> 
+  dplyr::mutate(sci = paste(Genus, Specific_epithet)) |> 
+  dplyr::left_join(sci_names_to_query) |> 
+  dplyr::group_by(sci, type) |>
+  dplyr::arrange(sci, type, -qual) |>
+  # slice first row for each species and call type. should be arranged so the first row has the highest quality
+  dplyr::slice_head(n = 1) |> 
+  dplyr::ungroup() |> 
+  dplyr::select(Recording_ID, common, type)
+
+setwd(here::here("recordings"))
+
+for( i in 1:nrow(download_these)){
+  warbleR::query_xc(
+    download = TRUE, 
+    X = slice(download_these, i), 
+    file.name = c("common", "type"))
+  print( paste("Finished", i, "of", nrow(download_these)))
+}
